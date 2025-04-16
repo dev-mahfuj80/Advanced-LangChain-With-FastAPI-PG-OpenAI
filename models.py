479ef77be4b952f8343716beda7877c089c27fb8
MdHelalUddinBiswas <helal@nexstack.sg>
2025-04-16 13:43:57 +0600

    show all data

--- models.py
+++ models.py
@@ -1,15 +1,40 @@
 import hashlib
 from typing import Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class DocumentResponse(BaseModel):
     page_content: str
     metadata: dict
 
+    @property
+    def room_number(self) -> str:
+        return self.metadata.get('room_number')
+
+    @property
+    def description(self) -> str:
+        return self.metadata.get('description')
+
+    @property
+    def room_size(self) -> float:
+        return self.metadata.get('room_size')
+
+    @property
+    def image_url(self) -> Optional[str]:
+        return self.metadata.get('image_url')
+
+    @property
+    def is_booked(self) -> bool:
+        return self.metadata.get('is_booked', False)
+
 
 class DocumentModel(BaseModel):
+    room_number: str
+    description: str
+    room_size: float
+    image_url: Optional[str] = None
+    is_booked: bool = False
     page_content: str
     metadata: Optional[dict] = {}
 
